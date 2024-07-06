a2aad332858eccd2a78daebc49a279b2935f2c62
emilydev <emilse.a.b@gmail.com>
2024-07-06 15:36:00 -0500

    se actualiza actividad

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,17 +7,17 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/falso-verdadero.svg", alt="Texto que describa la imagen")
+            img(src="@/assets/template/Arrastrar_Soltar.svg", alt="Actividad didáctica")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
-          h2 Nombre de la actividad
-        p.mb-4 #[b Objetivo] 
+          h2 Creación y administración de bases de datos SQL y NoSQL
+        p.mb-4 #[b Objetivo:] Aprender a configurar, diseñar y administrar bases de datos utilizando MySQL y MongoDB, aplicando conceptos de diseño conceptual, lógico y físico. 
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Tipo de actividad
+              p.fw-bold.mb-0 Relacionar términos
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
                 
